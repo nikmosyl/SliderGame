8ed17b9c44374a4b559792d41f3c4b85c1f529f0
Nikita Moskalev <nikmosyl@gmail.com>
2024-03-12 22:10:05 +0400

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by nikita on 12.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
